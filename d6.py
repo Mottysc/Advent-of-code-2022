77712c4de308fb4cc9ccfe446d2bded0ccf013a3
Motty S <mottyschneck8@gmail.com>
2022-12-07 22:28:17 +0200

    Day 6 complete

--- d6.py
+++ d6.py
@@ -3,12 +3,13 @@ letters = []
 
 for x in range(4, len(buffersteam)):
     current_letter = buffersteam[x]
-    if len(letters) < 4:
-        letters.extend(buffersteam[:x])
+    if len(letters) < 3:
+        letters.extend(buffersteam[:x-1])
     else:
+        letters.clear()
+        letters.extend(buffersteam[x-4:x])
         
-    
     if current_letter in letters:
         pass
     else:
-        print(buffersteam[x-4:x])
+        print(letters)
