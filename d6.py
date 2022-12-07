4e0d32af5ba6578e90e6f88cd9ad32f22f061a3f
Motty S <mottyschneck8@gmail.com>
2022-12-07 22:10:29 +0200

    next version

--- d6.py
+++ d6.py
@@ -1,9 +1,14 @@
 buffersteam = open("d6_input.txt").read()
+letters = []
 
-for x in range(4, len(buffersteam)-4):
-    letters = []
-    letters.extend(buffersteam[x-3:x])
-    if buffersteam[x] in letters:
+for x in range(4, len(buffersteam)):
+    current_letter = buffersteam[x]
+    if len(letters) < 4:
+        letters.extend(buffersteam[:x])
+    else:
+        
+    
+    if current_letter in letters:
         pass
     else:
-        print(buffersteam[x-3:x])
+        print(buffersteam[x-4:x])
