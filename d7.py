abae4a4e42be029dfddd201188ba78f9dcdf9148
Motty S <mottyschneck8@gmail.com>
2022-12-07 23:30:09 +0200

    tree maker

--- d7.py
+++ d7.py
@@ -1,14 +1,24 @@
 cmd_output = open("d7_input.txt").read().splitlines()
 
-filetree = []
-filesizes = []
+filetree = {}
+current_dir = []
 for line in cmd_output:
-    if line[0].isdigit():
-        size, filename = line.split(" ")
-        filesizes.append(size)
-totalsize = 0   
-for file in filesizes:
-    if int(file) < 100000:
-        totalsize += int(file)
+    command = line.split(" ")
+    if command[0] == "$":
+        if command[1] == "cd":
+            if command[2] != "..":
+                if len(current_dir) == 0:
+                    current_dir.append(command[2])
+                    filetree[command[2]] = []
+                else:
+                    current_dir.append(command[2])
+                    filetree[0][current_dir[-1]].append({command[2]: []})
+            else:
+                current_dir.pop()
+    elif command[0] == "dir":
+        if command[1] not in filetree[current_dir[-1]]:
+            filetree[current_dir[-1]].append({command[1]: []})
+    elif command[0].isdigit():
+        filetree[current_dir].append({command[1]: command[0]})
         
-print(totalsize)
+print(filetree)
