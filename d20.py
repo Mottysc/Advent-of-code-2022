1d39017a60fe5046a119f1c7dfbcd74b00bc3ac1
Motty S <mottyschneck8@gmail.com>
2023-01-03 21:00:24 +0200

    d20

--- d20.py
+++ d20.py
@@ -1,6 +1,14 @@
 encrypted_numbers = open("d20_input.txt").read().splitlines()
 encrypted_numbers = [int(x) for x in encrypted_numbers]
 
+class Number():
+    def __init__(self, number: int) -> None:
+        self.number = number
+        self.changed = False
 
-for x in encrypted_numbers:
+number_objs = [Number(x) for x in encrypted_numbers]
+print(number_objs)
+
+length_of_nums = len(number_objs)
+for x in range(length_of_nums):
     
